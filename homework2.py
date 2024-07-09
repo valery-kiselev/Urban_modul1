7b3b6b41f282ff1da7de81ee9543566ad4a53e36
valery-kiselev <83948789+valery-kiselev@users.noreply.github.com>
2024-07-09 21:02:02 +0400

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_of_completed_dz = 12
+number_hours_spent = 1.5
+course_name = 'Python'
+time_one_task = number_hours_spent / number_of_completed_dz
+print('Курс:', course_name, ',  всего задач:', number_of_completed_dz,',  затрачено часов:',  number_hours_spent, ',  среднее время выполнения:',  time_one_task)
